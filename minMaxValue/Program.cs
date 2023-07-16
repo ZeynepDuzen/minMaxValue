627be4833a7b33453485f05ed0e1f5f54a5c48c7
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-16 18:30:21 +0300

    Proje dosyası ekle.

---
+++ minMaxValue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minMaxValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(sbyte.MaxValue);
+            Console.WriteLine(sbyte.MinValue);
+
+            Console.Read();
+        }
+    }
+}
